0aa4a56cbe0de555d1d16cdde3ff4f3ec3213819
Souheil007 <107995350+Souheil007@users.noreply.github.com>
2026-09-11 11:24:52 +0100

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map <char , int> m ;
+        if (s.length() != t.length()) return false ;
+        for (int i=0 ; i < s.length();i++) { 
+            m[s[i]]++;
+            m[t[i]]--;
+        };
+        for (auto it = m.begin(); it != m.end(); ++it) {
+            if (it ->second != 0 )return false;
+        }
+        return true;
+    }
+};
