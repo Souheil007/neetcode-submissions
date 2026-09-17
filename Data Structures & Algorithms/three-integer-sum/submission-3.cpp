9dc2645bbe9dbe2939631a8a83f57fe262a59318
Souheil007 <107995350+Souheil007@users.noreply.github.com>
2026-09-17 11:15:42 +0100

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> v;
+        
+        set<vector<int>> vectorSet;
+        int x,y;
+        if(nums.size()==3){
+            if(nums[0]+nums[1]+nums[2]==0){
+                v.push_back(nums);
+                return v;
+            }
+        }
+        for (int j=0; j<nums.size()-2;j++){
+            unordered_map<int, int> m;
+            x=nums[j];
+            for (int i=j+1; i<nums.size();i++){
+                y=nums[i];
+                if (m.find(-(x+y)) != m.end()){
+                    vector<int> tempv;
+                    tempv.push_back(x);
+                    tempv.push_back(y);
+                    tempv.push_back(-(x+y));
+                    sort(tempv.begin(), tempv.end()); 
+                    vectorSet.insert(tempv);
+                } 
+                m.insert({y, i});
+            }
+        }
+        for (auto vec : vectorSet) { 
+            v.push_back(vec);
+        }
+        return v;
+    }
+};
