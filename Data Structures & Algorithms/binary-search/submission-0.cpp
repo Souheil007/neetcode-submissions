1fecee85e7fa7c77cf20b36cbc273cf2f259a810
Souheil007 <107995350+Souheil007@users.noreply.github.com>
2026-09-21 11:14:17 +0100

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int rsearch(vector<int>& nums, int l, int r, int target) {
+        if (l > r) {
+            return -1;
+        }
+
+        int mindex = l + (r - l) / 2;
+
+        if (nums[mindex] == target) {
+            return mindex;
+        }
+
+        if (nums[mindex] > target) {
+            return rsearch(nums, l, mindex - 1, target);
+        }
+
+        return rsearch(nums, mindex + 1, r, target);
+    }
+
+    int search(vector<int>& nums, int target) {
+        return rsearch(nums, 0, nums.size() - 1, target);
+    }
+};
