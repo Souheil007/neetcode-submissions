1e07a11bf7724fd55aebb0810f2c31c0691345ab
Souheil007 <107995350+Souheil007@users.noreply.github.com>
2026-09-22 20:01:15 +0100

    Add: remove-node-from-end-of-linked-list - submission-4

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-4.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr) {
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr ;
+            curr = temp;
+        }
+        return prev ;
+    }
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* reversed = reverseList(head);    
+        if (n == 1) {
+            reversed = reversed->next;
+        }
+        else {
+            ListNode* curr = reversed;
+            int i =1;
+            while (i<n-1){
+                curr = curr->next;
+                i++;
+            }
+            ListNode* second = curr->next->next;
+            curr->next = second;
+            
+            
+        }
+        reversed = reverseList(reversed);
+        return reversed;
+    }
+};
